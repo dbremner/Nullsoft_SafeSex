a7f758ce265cda0284c2749d95c7c4691980f27a
David Bremner <dbremner@gmail.com>
2015-10-10 15:10:50 -0700

    define WIN32_LEAN_AND_MEAN and specify additional headers explicitly, doing so saves +7mb in size of the precompiled header file

--- stdafx.h
+++ stdafx.h
@@ -1,7 +1,10 @@
 #pragma once
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <windowsx.h>
 #include <richedit.h>
+#include <shellapi.h>
+#include <commdlg.h>
 
 //project
 #include "blowfish.h"
